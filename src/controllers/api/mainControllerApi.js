3554bf436168c710d6019aa2cb20b12f0d3efabb
Martin93-css <tin.deunaa@gmail.com>
2023-05-16 17:45:44 -0300

    apis de producto terminada

--- src/controllers/api/mainControllerApi.js
+++ src/controllers/api/mainControllerApi.js
@@ -1,13 +1,42 @@
-const db = require('../../db/productos.json')
-
-console.log(db)
-
+const db = require('../../db/productos.json');
 
 const controller = {
-    index: (req , res) => {
-        res.render('home/index')
-    }
+    index: (req, res) => {
+        res.redirect("http://localhost:3005/api/productos");
+      }
+      
+  ,  
+  productos: (req, res) => {
+    const db = require('../../db/productos.json');
+
+    let productos = db.map(producto => {
+        return {
+            id: producto.id,
+            name: producto.nombre,
+            price: producto.precio,
+            discount: producto.descuento,
+            image: producto.imagen
+
+        }
+    })
+
+    let respuestaProducto = productos; 
+
+    let respuesta = {
+        meta: {
+          status: 200,
+          url: "http://localhost:3005/api/productos"
+        },
+        data: respuestaProducto
+   
+  }
+
+  res.json(respuesta)
+
+
+  
 }
 
+};
 
-module.exports = controller
+module.exports = controller;
--- src/routes/api/apiRouter.js
+++ src/routes/api/apiRouter.js
@@ -5,6 +5,7 @@ const mainController = require('../../controllers/api/mainControllerApi')
 
 
 router.get('/' , mainController.index )
+router.get('/productos' , mainController.productos )
 
 
 module.exports = router
